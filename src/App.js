d94d2a701b071e1f2fb07337c50173daee498ed1
Chris Patrick <atomize101@gmail.com>
2020-10-23 14:51:26 -0400

    fetch images from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import './App.css';
 
+//https://pixabay.com/api/
+
 export default function App() {
+	const [query, setQuery] = useState('');
+
+	const runQuery = () => {
+		fetch(`https://pixabay.com/api/?key=18830221-77b301a5cf4c8541d672fa5ad&q=pizza`);
+	};
 	return (
 		<div>
-			<input />
-			<button>Search</button>
+			<input onChange={(e) => setQuery(e.target.value)} />
+			<button onClick={runQuery}>Search</button>
 		</div>
 	);
 }
