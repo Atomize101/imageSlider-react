b159e12658bb3986b460f3d90334bfd350df89eb
Chris Patrick <atomize101@gmail.com>
2020-10-24 10:54:23 -0400

    Search now returns an image on submit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import './App.css';
 
 //https://pixabay.com/api/
 
 export default function App() {
 	const [query, setQuery] = useState('');
+	const [images, setImages] = useState([]);
 
 	const runQuery = () => {
-		fetch(`https://pixabay.com/api/?key=18830221-77b301a5cf4c8541d672fa5ad&q=pizza`);
+		fetch(`https://pixabay.com/api/?key=18830221-77b301a5cf4c8541d672fa5ad&q=${query}`).then((response) =>
+			response
+				.json()
+				.then(({ hits }) => hits.map(({ webformatURL }) => webformatURL))
+				.then(setImages)
+		);
 	};
+
 	return (
 		<div>
-			<input onChange={(e) => setQuery(e.target.value)} />
-			<button onClick={runQuery}>Search</button>
+			<div>
+				<input onChange={(e) => setQuery(e.target.value)} />
+				<button onClick={runQuery}>Search</button>
+			</div>
+			{images.length > 0 && <img src={images[0]} />}
 		</div>
 	);
 }
