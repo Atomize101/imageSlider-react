7b16488fb660e587d0e17fc29a54446b7495f442
Chris Patrick <atomize101@gmail.com>
2020-10-25 10:38:38 -0400

    Creating state

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function ImageSlider({ images }) {
-	return images.length > 0 && <img src={images[0]} />;
+	return (
+		images.length > 0 && (
+			<div>
+				<button>{'<'}</button>
+				<img src={images[0]} />
+				<button>{'>'}</button>
+			</div>
+		)
+	);
 }
